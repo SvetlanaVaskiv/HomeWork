cf95553d1891adc0725f893855262dce751b4234
Svetlana <svetanochka@gmail.com>
2021-10-03 23:29:24 +0300

    module

--- PingPong/index.js
+++ PingPong/index.js
@@ -20,6 +20,8 @@ function block(positionX, positionY, width, height, color) {
 let circleX = 30;
 let circleY = 250;
 let moveRight = true;
+
+
 let moveTop = true;
 let blockY = 200;
 let heightBlock = 100;
@@ -68,30 +70,9 @@ let playGame = setInterval(function () {
 	}
 }, 0)
 */
-class Point {
-	constructor(
-		x,
-		y) {
-		this._x = x;
-		this._y = y;
-	}
-	get x() {
-		return this._x;
-		;
-	}
 
-	set x(value) {
-		this._x = value
-	}
-	get y() {
-		return this._y;
-		;
-	}
 
-	set y(value) {
-		this._y = value;
-	}
-}
+
 class Platform {
 	constructor(
 		ctx,
@@ -140,23 +121,74 @@ class Platform {
 			this.height
 		);
 	}
-
 }
 
 
+
+class Block {
+	constructor(
+		positionX,
+		positionY,
+		width,
+		height,
+	) {
+		this._positionX = positionX;
+		this._positionY = positionY;
+		this._width = width;
+		this._height = height;
+	}
+	get positionX() {
+		return this._positionX;
+	}
+	get positionY() {
+		return this._positionY;
+	}
+	get width() {
+		return this._width;
+	}
+	get height() {
+		return this._height;
+	}
+
+	set positionX(value) {
+		this._positionX = value;
+	}
+	set positionY(value) {
+		this._positionY = value;
+	}
+
+}
+const background = new Image();
+background.src = "image/background.jpg"
 class Field {
 	constructor(
+		background,
+		y = 45,
 		width = 1000,
 		height = 800,
 		score = 0,
 
+
 	) {
+		this._background = background;
+		this._y = y;
 		this._width = width;
 		this._height = height;
 		this._score = score;
 
 
 	}
+	get background() {
+		return this._background
+	}
+	get y() {
+		return this._y
+	}
+	set y(value) {
+		return this._y
+	}
+
+
 	get width() {
 		return this._width;
 		;
@@ -180,34 +212,30 @@ class Field {
 		this._score = value;
 	}
 	render() {
-		ctx.clearRect(0, 0, 1000, 800)
-		ctx.rect(0, 0, this.width, this.height);
-		ctx.fillStyle = "#FCC";
-		ctx.font = " 300px Arial";
-		ctx.fillText(this.score, 500, 400);
-		ctx.stroke();
+		ctx.drawImage(background, 0, 45, 1000, 800, 0, 45, 1000, 800)
 
+		ctx.rect(0, this.y, this.width, this.height);
+		ctx.stroke();
+		ctx.font = "bold 50px serif";
+		ctx.fillStyle = "#63C";
+		ctx.fillText(" Total: " + this.score, 0, 40)
 	}
+
 }
 class Ball {
 	constructor(
-		ctx,
+
 		positionX,
 		positionY,
 		diametr,
-		lineWidth = 2,
-		color,
 	) {
-		this._ctx = ctx;
 		this._positionX = positionX;
 		this._positionY = positionY;
 		this._diametr = diametr;
-		this._lineWidth = lineWidth;
-		this._color = color;
 	}
 	get radius() {
 
-		return this._diametr / 2 - this._lineWidth;
+		return this._diametr / 2;
 	}
 	get positionX() {
 		return this._positionX;
@@ -215,9 +243,7 @@ class Ball {
 	get positionY() {
 		return this._positionY;
 	}
-	get color() {
-		return this._color;
-	}
+
 	set positionX(value) {
 		this._positionX = value;
 	}
@@ -228,193 +254,366 @@ class Ball {
 		this._diametr = value;
 	}
 	render() {
-		this._ctx.fillStyle = this._color;
-		this._ctx.beginPath();
+		ctx.beginPath();
+		ctx.fillStyle = '#C3C';
 
-
-		const x = this._positionX;
-		const y = this._positionY;
-		this._ctx.arc(
-			x,
-			y,
-			this.radius,
+		ctx.arc(
+			ball.positionX,
+			ball.positionY,
+			ball.radius,
 			0,
 			Math.PI * 2, true
 		);
-
-		this._ctx.fill();
-
+		ctx.fill();
 	}
 
-
 }
-const ball = new Ball(ctx, 30, 250, 40, 2, 'red');
-let platform = new Platform(ctx, 600, 760, 120, 35, '#336');
-const field = new Field;
+
 let moveDown = true;
 let moveRight = true;
 let newPosX;
+const levelComplete = new Audio('Audio/level_complete.mp3');
+const game_complete = new Audio('Audio/game_complete.mp3');
+const intro = new Audio('Audio/intro.mp3');
+const light_damage1 = new Audio('Audio/light-damage-a1.mp3');
+const ball_bounce = new Audio('Audio/ball_bounce.mp3');
+
+
+let lifeImage = new Image();
+let lifeScore = [];
+lifeImage.src = "image/heart.png";
+for (let i = 0; i < 3; i++) {
+	lifeScore.push(lifeImage)
+};
+let field = new Field();
+const platform = new Platform(ctx, 600, 780, 200, 35, '#336');
+let ball = new Ball(300, 250, 60,);
+let block;
+let blocksX = [];
+let blocksY = [];
+let blocksT = [];
+let blocksE = [];
+let blocksA = [];
+let blocksB = [];
+for (let i = 0; i < 7; i++) {
+	block = new Block((150 * i) + (i * 2), 50 + i * 65, 90, 90)
+	blocksB.push(block)
+
+}
+
+for (let i = 0; i < 7; i++) {
+	block = new Block((150 * i) + (i * 2), 450 - i * 65, 90, 90)
+	blocksA.push(block)
+
+}
+
+for (let i = 0; i < 10; i++) {
+	block = new Block((102 * i) + (i * 2), 550, 30, 35)
+	blocksE.push(block)
+
+}
+for (let i = 1; i <= 10; i++) {
+	block = new Block((90 * i) + (i * 2), 100, 80, 35);
+	blocksX.push(block)
+
+}
+for (let i = 0; i < 7; i++) {
+	block = new Block((102 * i) + (i * 45), 250, 100, 35)
+	blocksY.push(block)
+
+}
+for (let i = 0; i < 10; i++) {
+	block = new Block((102 * i) + (i * 2), 450, 40, 35,)
+	blocksT.push(block)
+
+}
+
 document.addEventListener('keydown', function (event) {
 	if (event.code == "ArrowRight" && platform.positionX + platform.width !== 1000) {
-		newPosX = platform.positionX += 10;
+		newPosX = platform.positionX += 20;
 	} else if (event.code == "ArrowLeft" && platform.positionX != 0) {
-		newPosX = platform.positionX -= 10;
+		newPosX = platform.positionX -= 20;
 	}
 });
 
-
-
-let pointx = [];
-for (let i = 0; i < 15; i++) {
-	let j = Math.floor(Math.random() * 1000);
-	pointx.push(j)
+function drawBlock(x, y, width, height,) {
+	ctx.beginPath();
+	ctx.fillStyle = '#6F3';
+	ctx.fillRect(
+		x = block.positionX,
+		y = block.positionY,
+		width = block.width,
+		height = block.height
+	);
+	ctx.fill()
 }
-let WIDTH = platform.width * Math.floor(Math.random() * 10)
-let pointy = [];
-for (let i = 0; i < 15; i++) {
-	let k = Math.floor(Math.random() * 100);
-	pointy.push(k)
+function drawImages() {
+	ctx.beginPath();
+
+	for (let i = 0; i < lifeScore.length; i++) {
+		ctx.drawImage(lifeImage, (65 * i) + 800, 0);
+	}
+
 }
 
+const AngryHeart = new Image();
+AngryHeart.src = "image/heartNegative.png";
+const song = new Audio('Audio/song.mp3')
+const on = document.getElementById('on');
+const off = document.getElementById('off');
 
 
-let platforms = new Platform(ctx, pointx[0], pointy[0], WIDTH, 35, '#CCC');
-let platforms1 = new Platform(ctx, pointx[1], pointy[1], WIDTH, 35, '#CCC');
-let platforms2 = new Platform(ctx, pointx[2], pointy[2], WIDTH, 35, '#CCC');
-let platforms3 = new Platform(ctx, pointx[3], pointy[3], WIDTH, 35, '#CCC');
-let platforms4 = new Platform(ctx, pointx[4], pointy[4], WIDTH, 35, '#CCC');
-let platforms5 = new Platform(ctx, pointx[5], pointy[5], WIDTH, 35, '#CCC');
-let platforms6 = new Platform(ctx, pointx[6], pointy[6], WIDTH, 35, '#CCC');
-let platforms7 = new Platform(ctx, pointx[7], pointy[7], WIDTH, 35, '#CCC');
-let platforms8 = new Platform(ctx, pointx[8], pointy[8], WIDTH, 35, '#CCC');
-let platforms9 = new Platform(ctx, pointx[9], pointy[9], WIDTH, 35, '#CCC');
+
+
+
+
 
 let playGame = setInterval(function () {
+	ctx.clearRect(0, 0, field.width, field.height + field.y + ball.radius);
+	song.play()
+	field.render();
+	ball.render(600)
+	for (block of blocksX) (
+		drawBlock());
+	for (block of blocksY) (
+		drawBlock()
+	);
+	platform.render();
+	for (lifeImage of lifeScore) {
+		drawImages()
+	}
+
+
+
+
+
+	let minusLife = lifeScore.length + 1;
+
+
 
 	if (ball.positionX >= newPosX
-		&& ball.positionX <= newPosX + 120
-		&& ball.positionY + 16 === platform.positionY
-	) {
+		&& ball.positionX <= newPosX + platform.width
+		&& ball.positionY + ball.radius === platform.positionY
 
+	) {
+		ball_bounce.play()
+		moveDown = true;
+		field.score++;
+	} else if (ball.positionY + ball.radius === platform.positionY
+		&& ball.positionX >= platform.positionX
+		&& ball.positionX <= platform.positionX + platform.width
+
+	) {
+		ball_bounce.play()
 		moveDown = true;
 		field.score++;
 	}
-	//	switch (ball.positionY) {
-	//	case pointy[0]: case pointy[1]: case pointy[2]: case pointy[3]:
-	//case pointy[4]: case pointy[5]: case pointy[6]: case pointy[7]:
-	//	case pointy[8]: case pointy[9]:
-	//	moveDown = false;
-	//  }
 
-	if (ball.positionX >= platforms.positionX
-		&& ball.positionX <= platforms.positionX + WIDTH
-		&& ball.positionY - ball.radius === platforms.positionY + platforms.height
-	) {
+	for (block of blocksT) (
+		drawBlock());
+	let deletT = 0;
+	for (block of blocksT) {
+		if (ball.positionX >= block.positionX
+			&& ball.positionX <= block.positionX + block.width
+			&& ball.positionY - ball.radius <= block.positionY + block.height
+		) {
+			light_damage1.play();
 
-		moveDown = false;
+			blocksT.splice(deletT, 1)
+			moveDown = false;
+		}
+		deletT++;
+	}
+
+	let deletY = 0;
+	for (block of blocksY) {
+		if (ball.positionX >= block.positionX
+			&& ball.positionX <= block.positionX + block.width
+			&& ball.positionY - ball.radius <= block.positionY + block.height
+		) {
+			light_damage1.play();
+
+			blocksY.splice(deletY, 1)
+			moveDown = false;
+		}
+		deletY++;
+	}
+	let deletX = 0;
+	for (block of blocksX) {
+		if (ball.positionX >= block.positionX
+			&& ball.positionX <= block.positionX + block.width
+			&& ball.positionY - ball.radius <= block.positionY + block.height
+		) {
+			light_damage1.play();
+			blocksX.splice(deletX, 1);
+			moveDown = false;
+		}
+		deletX++;
 	}
 
 
-	if (ball.positionX >= platforms1.positionX
-		&& ball.positionX <= platforms1.positionX + WIDTH
-		&& ball.positionY - ball.radius === platforms1.positionY + platforms1.height) {
 
-		moveDown = false;
-	} if (ball.positionX >= platforms2.positionX
-		&& ball.positionX <= platforms2.positionX + WIDTH
-		&& ball.positionY - ball.radius === platforms2.positionY + platforms2.height) {
 
-		moveDown = false;
-	} if (ball.positionX >= platforms3.positionX
-		&& ball.positionX <= platforms3.positionX + WIDTH
-		&& ball.positionY - ball.radius === platforms3.positionY + platforms3.height) {
+	if (blocksY.length === 0 && blocksT.length === 0 && blocksX.length === 0) {
+		for (block of blocksE) (
+			drawBlock()
 
-		moveDown = false;
-	} if (ball.positionX >= platforms4.positionX
-		&& ball.positionX <= platforms4.positionX + WIDTH
-		&& ball.positionY - ball.radius === platforms4.positionY + platforms4.height) {
+		);
+		for (block of blocksA) (
+			drawBlock()
 
-		moveDown = false;
-	} if (ball.positionX >= platforms5.positionX
-		&& ball.positionX <= platforms5.positionX + WIDTH
-		&& ball.positionY - ball.radius === platforms5.positionY + platforms5.height) {
+		);
+		for (block of blocksB) (
+			drawBlock()
 
-		moveDown = false;
-	} if (ball.positionX >= platforms6.positionX
-		&& ball.positionX <= platforms6.positionX + WIDTH
-		&& ball.positionY - ball.radius === platforms6.positionY + platforms6.height) {
+		);
 
-		moveDown = false;
-	} if (ball.positionX >= platforms7.positionX
-		&& ball.positionX <= platforms7.positionX + WIDTH
-		&& ball.positionY - ball.radius === platforms7.positionY + platforms7.height
-	) {
+
+		let deletA = 0;
+		for (block of blocksA) {
+			if (ball.positionX + ball.radius >= block.positionX
+				&& ball.positionX - ball.radius <= block.positionX + block.width + 1
+				&& ball.positionY - ball.radius === block.positionY + block.height
+			) {
+				light_damage1.play();
+
+				blocksA.splice(deletA, 1)
+				moveDown = false;
+			}
+			else if (ball.positionX - ball.radius > block.positionX
+				&& ball.positionX - ball.radius < block.positionX + block.width
+				&& ball.positionY + ball.radius === block.positionY
+
+			) {
+				light_damage1.play();
+
+				blocksA.splice(deletA, 1)
+				moveDown = false;
+			}
+			deletA++;
+		}
+		let deletE = 0;
+		for (block of blocksE) {
+			if (ball.positionX + ball.radius >= block.positionX
+				&& ball.positionX - ball.radius <= block.positionX + block.width + 1
+				&& ball.positionY - ball.radius === block.positionY + block.height
+
+			) {
+				light_damage1.play();
+
+				blocksE.splice(deletE, 1)
+				moveDown = false;
+			}
+			else if (ball.positionX + ball.radius > block.positionX
+				&& ball.positionX - ball.radius < block.positionX + block.width
+				&& ball.positionY + ball.radius === block.positionY
+
+			) {
+				light_damage1.play();
+
+				blocksE.splice(deletE, 1)
+				moveDown = false;
+			}
+			deletE++;
+
+
+		}
+
+		let deletB = 0;
+		for (block of blocksB) {
+			if (ball.positionX + ball.radius >= block.positionX
+				&& ball.positionX - ball.radius <= block.positionX + block.width + 1
+				&& ball.positionY - ball.radius === block.positionY + block.height
+
+			) {
+				light_damage1.play();
+
+				blocksB.splice(deletB, 1)
+				moveDown = false;
+			} else if (ball.positionX - ball.radius > block.positionX
+				&& ball.positionX - ball.radius < block.positionX + block.width
+				&& ball.positionY + ball.radius === block.positionY
+
+			) {
+				light_damage1.play();
+
+				blocksB.splice(deletB, 1)
+				moveDown = false;
+			}
+			deletB++;
+		}
+	}
+
+
+
+	if (ball.positionY > platform.positionY + ball.radius) {
+		ball.positionY = platform.positionY - ball.radius * 2;
+		ball.positionX = newPosX + platform.width / 2
+			|| platform.positionX + platform.width / 2;
+		moveDown = true;
+		lifeScore.splice(0, 1);
+		minusLife--;
+
+		if (!minusLife) {
+			clearInterval(playGame);
+			song.pause()
+
+			intro.play()
+			ctx.drawImage(AngryHeart, 250, 100);
+			ctx.fillStyle = '#90C'
+			ctx.font = " 180px serif";
+			ctx.fillText('Game over', 120, 700);
+
+			setTimeout(function () {
+				document.location.reload()
+			}, 14000)
+		}
+	}
+	let count = blocksA.length + blocksB.length + blocksE.length;
+	if (!count) {
+		song.pause()
+
+		game_complete.play()
+		ctx.beginPath()
+		ctx.fillStyle = '#90C'
+		ctx.font = " 100px serif";
+		ctx.fillText('Congratulations,', 160, 300);
+		ctx.fillText(' you are a winner !!!', 120, 600);
+		clearInterval(playGame);
+
+		setTimeout(function () {
+			document.location.reload()
+		}, 11000)
+
+	}
+
+
+	if (ball.positionY === field.y + ball.radius) {
+		ball_bounce.play()
 
 		moveDown = false;
 	}
-
-	if (ball.positionX >= platforms8.positionX
-		&& ball.positionX <= platforms8.positionX + WIDTH
-		&& ball.positionY - ball.radius == platforms8.positionY + platforms8.height) {
-
-		moveDown = false;
-	} if (ball.positionX >= platforms9.positionX
-		&& ball.positionX <= platforms9.positionX + WIDTH
-		&& ball.positionY - ball.radius === platforms9.positionY + platforms9.height
-		&& ball.positionY + ball.radius === 0
-	) {
-
-		moveDown = false;
-	}
-	if (ball.positionY === ball.radius) {
-		moveDown = false;
-	}
-	if (ball.positionX === 985) {
+	if (ball.positionX === field.width - ball.radius) {
+		ball_bounce.play()
 		moveRight = false;
-
 	}
 	if (ball.positionX === ball.radius) {
+		ball_bounce.play()
 		moveRight = true;
-
 	}
 	if (moveRight === true) {
-		ball.positionX++;
-
+		ball.positionX += 5;
 	} else {
-		ball.positionX--;
-
+		ball.positionX -= 5;
 	}
 	if (moveDown === true) {
-		ball.positionY--;
-
+		ball.positionY -= 5;
 	} else {
-		ball.positionY++;
-
+		ball.positionY += 5;
 	}
 
 
-
-	field.render();
-	ball.render();
-	platform.render();
-	platforms.render()
-	platforms1.render()
-	platforms2.render()
-	platforms3.render()
-	platforms4.render()
-	platforms5.render()
-	platforms6.render()
-	platforms7.render()
-	platforms8.render()
-	platforms9.render()
-	if (ball.positionY > platform.positionY) {
-		clearInterval(playGame);
-	}
-}, 5)
-
-
-
-
+}, 20)
 
 
 
